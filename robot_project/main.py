803b94e6e382b38c1f4ccf62347bd0bb4fccbe40
lochungtin <lochungtin@gmail.com>
2022-09-02 11:16:04 +0100

    update robot project

--- robot_project/main.py
+++ robot_project/main.py
@@ -6,6 +6,8 @@ grid_size = 10
 # robot current coordinates
 row_index = randint(0, grid_size - 1)
 col_index = randint(0, grid_size - 1)
+prev_row_index = row_index
+prev_col_index = col_index
 
 # quadrant indentifiers
 quadrant_prompt = ["top left", "top right", "bottom left", "bottom right"]
@@ -18,6 +20,9 @@ direction_prompt = ["North", "South", "West", "East"]
 direction_indices = {"n": 0, "s": 1, "w": 2, "e": 3}
 # robot current direction
 direction = randint(0, 3)
+
+# turning table
+clockwise_turning_table = {0: 3, 3: 1, 1: 2, 2: 0}
 
 
 def regulatePosition(value):
@@ -45,28 +50,45 @@ def updateQuadrantIndex():
 def printLocation():
     """Print location information"""
     print(
-        "My current location is ({}, {}). I am in the {} quadrant".format(
-            row_index, col_index, quadrant_prompt[quadrant_index]
+        "My current location is ({}, {}), facing {}.".format(
+            row_index, col_index, direction_prompt[direction]
         )
     )
 
 
 def move():
     """Update robot location by 1 step"""
-    global row_index, col_index
+    global row_index, col_index, prev_row_index, prev_col_index
 
     if direction < 2:
         # branchless if statement numeric hack
         # if direction == 0, row_index -= 1, else row_index += 1
+        prev_row_index = row_index
         row_index = regulatePosition(row_index + (2 * direction - 1))
     else:
         # branchless if statement numeric hack
         # if direction == 2, col_index -= 1, else col_index += 1
+        prev_col_index = col_index
         col_index = regulatePosition(col_index + (2 * direction - 5))
 
-    updateQuadrantIndex()
+    if prev_row_index == row_index and prev_col_index == col_index:
+        print("I have a wall in front of me!")
+        return False
+    else:
+        updateQuadrantIndex()
 
-    print("Moving one step forward.")
+        print("Moving one step forward.")
+        printLocation()
+
+        return True
+
+
+def turnClockwise():
+    global direction
+
+    direction = clockwise_turning_table[direction]
+
+    print("Turning 90 degrees clockwise.")
     printLocation()
 
 
@@ -88,6 +110,16 @@ updateQuadrantIndex()
 print("Hello. My name is {}. My ID is 1000.".format(name))
 printLocation()
 
-print("I am facing {}.".format(direction_prompt[direction]))
+reached = False
+while True:
+    while move():
+        if row_index == grid_size - 1 and col_index == grid_size - 1:
+            reached = True
+            break
 
-move()
+    if reached:
+        break
+
+    turnClockwise()
+
+print("I am drinking Ribena! I am happy!")
