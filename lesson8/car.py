9e6f11935c15427007d876915da5d90bcba46a3e
lochungtin <lochungtin@gmail.com>
2022-09-02 21:17:09 +0100

    lesson 8

--- lesson8/car.py
+++ lesson8/car.py
@@ -9,12 +9,61 @@ class Car:
         self.speed = speed
         self.rotation = rotation
 
+    def accelerate(self, rate=1):
+        self.speed += rate
+
+    def decelerate(self, rate=1):
+        self.speed = max(self.speed - rate, 0)
+
+    def brake(self):
+        self.speed = 0
+
+    def steer(self, angle):
+        self.rotation += angle
+
+        if self.rotation > 180:
+            self.rotation = 180
+        elif self.rotation < -180:
+            self.rotation = -180
+
 
 personal_car = Car("Honda Civic", 2015, False)
+
 print(personal_car)
 print(personal_car.model)
 print(personal_car.year)
 print(personal_car.is_manual)
 print(personal_car.max_passengers)
 print(personal_car.speed)
+print(personal_car.rotation)
+print()
+
+personal_car.accelerate()
+print(personal_car.speed)
+
+personal_car.accelerate(5)
+print(personal_car.speed)
+
+personal_car.decelerate()
+print(personal_car.speed)
+
+personal_car.decelerate(20)
+print(personal_car.speed)
+
+personal_car.accelerate(10)
+print(personal_car.speed)
+
+personal_car.brake()
+print(personal_car.speed)
+
+personal_car.steer(30)
+print(personal_car.rotation)
+
+personal_car.steer(200)
+print(personal_car.rotation)
+
+personal_car.steer(-200)
+print(personal_car.rotation)
+
+personal_car.steer(-200)
 print(personal_car.rotation)
