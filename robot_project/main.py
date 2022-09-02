3186afc27940c46519613321c0db7d12def380fa
lochungtin <lochungtin@gmail.com>
2022-09-02 21:51:05 +0100

    add object methods

--- robot_project/main.py
+++ robot_project/main.py
@@ -39,6 +39,7 @@ def setup_robot(grid_size, robot_index):
         base_id + robot_index,
         (randint(0, grid_size - 1), randint(0, grid_size - 1)),
         randint(0, 3),
+        grid_size,
     )
 
 
@@ -46,88 +47,19 @@ def run_simulation(robot, destination):
     """Start robot navigation simulation.
 
     Args:
-        robot (dict[str, any]): robot current data
+        robot (Robot): robot current data
         destination (Tuple[int, int]): robot's destination
     """
-    print("\n{} is searching for its drink.".format(robot.name))
+    robot.print_starting_message()
 
-    while robot.position != destination:
-        if navigate(robot):
-            print("Moving one step foward.")
-            print_location_data(robot)
+    robot.set_destination(destination)
+    while not robot.is_at_destination():
+        if robot.move_foward():
+            robot.print_location_message()
         else:
-            print("I have a wall in front of me!\nTurning 90 degrees clockwise.")
-            rotate_robot(robot)
+            robot.turn_right()
 
-    print("I am drinking Ribena! I am happy!")
-
-
-def print_robot_greeting(robot):
-    """Print geeting message
-
-    Args:
-        robot (dict[str, any]): robot current data
-    """
-    print("Hello. My name is {}. My ID is {}.".format(robot.name, robot.id))
-
-
-def navigate(robot):
-    """Attempt to move robot forward.
-
-    Args:
-        robot (dict[str, any]): robot current data
-
-    Returns:
-        bool: whether the robot has moved
-    """
-    prev_row, prev_col = robot.position
-    row, col = robot.position
-
-    if robot.direction < 2:
-        row = regulate_position(prev_row + (2 * robot.direction - 1), prev_row)
-    else:
-        col = regulate_position(prev_col + (2 * robot.direction - 5), prev_col)
-
-    robot.position = (row, col)
-
-    return prev_row != row or prev_col != col
-
-
-def regulate_position(new_value, old_value):
-    """Check if new position is within the grid boundaries.
-
-    Args:
-        new_value (int): new position value
-        old_value (int): old position value
-
-    Returns:
-        int: valid position value
-    """
-    if new_value < 0 or new_value >= grid_size:
-        return old_value
-    return new_value
-
-
-def print_location_data(robot):
-    """Print the location data
-
-    Args:
-        robot (dict[str, any]): robot current data
-    """
-    print(
-        "My current location is {}, facing {}".format(
-            robot.position, directions[robot.direction]
-        )
-    )
-
-
-def rotate_robot(robot):
-    """Rotates the robot 90 degrees clockwise.
-
-    Args:
-        robot (dict[str, any]): robot current data
-    """
-    robot.direction = clockwise_rotation_table[robot.direction]
+    robot.print_ending_message()
 
 
 robots = 3
@@ -135,7 +67,7 @@ robot_data = []
 for index, destination in enumerate(choices(destinations, k=robots)):
     robot = setup_robot(grid_size, index)
     robot_data.append((robot, destination))
-    print_robot_greeting(robot)
+    robot.print_greeting_message()
 
 for data in robot_data:
     robot, destination = data
--- robot_project/robot.py
+++ robot_project/robot.py
@@ -1,6 +1,64 @@
+clockwise_rotation_table = {0: 3, 3: 1, 1: 2, 2: 0}
+directions = ["North", "South", "West", "East"]
+
+
 class Robot:
-    def __init__(self, name, idnum, position, direction):
+    def __init__(self, name, idnum, position, direction, grid_size):
         self.name = name
         self.id = idnum
         self.position = position
         self.direction = direction
+        self.grid_max = grid_size - 1
+
+    # messages
+    def print_greeting_message(self):
+        print("Hello. My name is {}. My ID is {}.".format(self.name, self.id))
+
+    def print_location_message(self):
+        print(
+            "Moving one step foward.\nMy current location is {}, facing {}".format(
+                self.position, directions[self.direction]
+            )
+        )
+
+    def print_starting_message(self):
+        print("\n{} is searching for its drink.".format(self.name))
+
+    def print_ending_message(self):
+        print("I am drinking Ribena! I am happy!")
+
+    # set destination
+    def set_destination(self, destination):
+        self.destination = destination
+
+    # status queries
+    def is_at_destination(self):
+        return self.position == self.destination
+
+    # movement actions
+    def move_foward(self):
+        prev_row, prev_col = self.position
+        row, col = self.position
+
+        if self.direction < 2:
+            row = self.regulate_position_value(row + 2 * self.direction - 1)
+        else:
+            col = self.regulate_position_value(col + 2 * self.direction - 5)
+
+        self.position = (row, col)
+
+        return row != prev_row or col != prev_col
+
+    def turn_right(self):
+        self.direction = clockwise_rotation_table[self.direction]
+
+        print("I have a wall in front of me!\nTurning 90 degrees clockwise.")
+
+    # utility functions
+    def regulate_position_value(self, value):
+        if value < 0:
+            return 0
+        if value > self.grid_max:
+            return self.grid_max
+
+        return value
