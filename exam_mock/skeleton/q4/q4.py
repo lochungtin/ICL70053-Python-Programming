613fe7a89a9e857620b6641655cfcf1cc2c5bb9e
lochungtin <lochungtin@gmail.com>
2022-12-10 01:40:42 +0000

    update

--- exam_mock/skeleton/q4/q4.py
+++ exam_mock/skeleton/q4/q4.py
@@ -20,15 +20,11 @@
 
 def get_winner(board):
     l = len(board)
-    for i in range(l):
-        if len(set(board[i][j] for j in range(l))) == 1 and board[i][0] != " ":
-            return board[i][0]
-        if len(set(board[j][i] for j in range(l))) == 1 and board[0][i] != " ":
-            return board[0][i]
-    if len(set(board[i][i] for i in range(l))) == 1 and board[0][0] != " ":
-        return board[0][0]
-    if len(set(board[i][l - i - 1] for i in range(l))) == 1 and board[0][l - 1] != " ":
-        return board[0][l - 1]
+    s = [set(board[i][i] for i in range(l)), set(board[i][l - i - 1] for i in range(l))]
+    s += [set(board[j][i] for j in range(l)) for i in range(l)]
+    s += [set(board[i][j] for j in range(l)) for i in range(l)]
+    for w in [list(r)[0] for r in s if len(r) == 1 and " " not in r]:
+        return w
     if any(map(lambda x: x == " ", [c for r in board for c in r])):
         return None
     return "draw"
